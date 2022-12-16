af03945f5a7e41b88f291c85ddc69858f8a9f040
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2022-12-16 21:38:16 +0100

    Add InvestBlock Template

---
+++ src/components/templates/InvestBlock.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="search-input-container">
+    <img src="@/assets/img/search.svg" alt="" />
+    <input type="text" name="search" id="search" placeholder="Search" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
