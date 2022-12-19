e4d0d8afb43056d0881862cd2005a755d2c320c4
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2022-12-19 08:12:15 +0100

    Change SummaryChart from image to chart

--- frontend/src/components/organizms/SummaryChart.vue
+++ frontend/src/components/organizms/SummaryChart.vue
@@ -1,49 +1,50 @@
 <template>
   <invest-block class="relative !pb-0" title="Summary">
-    <!--<canvas id="summaryChart"></canvas>-->
-    <img src="@/assets/img/summary-chart.svg" alt="" class="summary-chart" />
+    <canvas class="summary-chart" ref="summaryChart"></canvas>
   </invest-block>
 </template>
 
 <script setup>
-// import Chart from "chart.js/auto";
-// import { onMounted } from "vue";
-//
-// onMounted(() => {
-//   const ctx = document.getElementById("summaryChart");
-//
-//   // const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-//   // gradient.addColorStop(0, "rgba(250,174,50,1)");
-//   // gradient.addColorStop(1, "rgba(250,174,50,0)");
-//
-//   new Chart(ctx, {
-//     type: "line",
-//     data: {
-//       labels: ["", "", "", "", "", ""],
-//       datasets: [
-//         {
-//           label: "This month",
-//           data: [12, 19, 3, 5, 2, 3],
-//           borderDash: [4, 16],
-//           borderDashOffset: 4,
-//           borderWidth: 2,
-//           borderColor: "#7445FB",
-//           fill: true,
-//           backgroundColor: "rgba(255, 99, 132, 0.2)",
-//           pointStyle: false,
-//           tension: 1,
-//         },
-//       ],
-//     },
-//     options: {
-//       scales: {
-//         y: {
-//           beginAtZero: true,
-//         },
-//       },
-//     },
-//   });
-// });
+import Chart from "chart.js/auto";
+import { onMounted, ref } from "vue";
+
+const summaryChart = ref(null);
+
+onMounted(() => {
+  const ctx = summaryChart.value;
+  console.log(ctx);
+  // const gradient = ctx.createLinearGradient(0, 0, 0, 400);
+  // gradient.addColorStop(0, "rgba(250,174,50,1)");
+  // gradient.addColorStop(1, "rgba(250,174,50,0)");
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: ["", "", "", "", "", ""],
+      datasets: [
+        {
+          label: "This month",
+          data: [12, 19, 3, 5, 2, 3],
+          borderDash: [4, 16],
+          borderDashOffset: 4,
+          borderWidth: 2,
+          borderColor: "#7445FB",
+          fill: true,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          pointStyle: false,
+          tension: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
+});
 </script>
 
 <style lang="scss">
