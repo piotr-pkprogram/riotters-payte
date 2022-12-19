ddeb55828dd867a7d4832ccf2913b7a482babda1
Piotr385 <74099800+Piotr385@users.noreply.github.com>
2022-12-19 09:44:32 +0100

    Drawing a chart using chart.js in SummaryChart component

--- frontend/src/components/organizms/SummaryChart.vue
+++ frontend/src/components/organizms/SummaryChart.vue
@@ -6,49 +6,95 @@
 
 <script setup>
 import Chart from "chart.js/auto";
-import { onMounted, ref } from "vue";
+import { ref, watchEffect } from "vue";
 
 const summaryChart = ref(null);
 
-onMounted(() => {
-  const ctx = summaryChart.value;
-  console.log(ctx);
-  // const gradient = ctx.createLinearGradient(0, 0, 0, 400);
-  // gradient.addColorStop(0, "rgba(250,174,50,1)");
-  // gradient.addColorStop(1, "rgba(250,174,50,0)");
+watchEffect(() => {
+  if (summaryChart.value) {
+    const ctx = summaryChart.value;
+    // const gradient = ctx.createLinearGradient(116, 69, 251, 40);
+    // gradient.addColorStop(0, "rgba(250,174,50,1)");
+    // gradient.addColorStop(1, "rgba(250,174,50,0)");
 
-  new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: ["", "", "", "", "", ""],
-      datasets: [
-        {
-          label: "This month",
-          data: [12, 19, 3, 5, 2, 3],
-          borderDash: [4, 16],
-          borderDashOffset: 4,
-          borderWidth: 2,
-          borderColor: "#7445FB",
-          fill: true,
-          backgroundColor: "rgba(255, 99, 132, 0.2)",
-          pointStyle: false,
-          tension: 1,
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: ["", "", "", "", "", ""],
+        datasets: [
+          {
+            label: "This month",
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 2,
+            borderColor: "#7445FB",
+            fill: true,
+            backgroundColor: "rgba(116, 69, 251, 0.4)",
+            tension: 1,
+            borderJoinStyle: "miter",
+            pointBackgroundColor: "#F7F7F9",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "#fff",
+            pointHoverBorderColor: "#7445FB",
+          },
+          {
+            label: "Last month",
+            data: [6, 12, 8, 2, 9, 3],
+            borderDash: [6, 3],
+            borderDashOffset: 4,
+            borderWidth: 2,
+            borderColor: "#858585",
+            pointStyle: false,
+            tension: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            labels: {
+              // This more specific font property overrides the global property
+              font: {
+                size: 12,
+                family: "'Poppins', Arial, sans-serif",
+              },
+            },
+          },
         },
-      ],
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true,
+        scales: {
+          x: {
+            border: {
+              display: false,
+            },
+            drawTicks: false,
+            grid: {
+              borderDash: [8, 4],
+              color: "#F7F7F9",
+            },
+          },
+          y: {
+            beginAtZero: true,
+            border: {
+              display: false,
+            },
+            grid: {
+              color: "#858585",
+            },
+            tickBorderDash: [6, 3],
+            tickBorderDashOffset: 4,
+          },
         },
       },
-    },
-  });
+    });
+  } else {
+    // not mounted yet, or the element was unmounted (e.g. by v-if)
+  }
 });
 </script>
 
 <style lang="scss">
 .summary-chart {
-  @apply w-full mt-6 bottom-4 left-0 lgs:absolute;
+  @apply w-full h-full mt-6;
 }
 </style>
